46ed8645ea5759fc96a5eee3d2829ce51d271634
Anchanajeyakumar <anchanajeyakumar@gmail.com>
2024-10-08 15:37:39 +0530

    Hello

---
+++ githubintegration/src/githubintegration/project1.java
@@ -0,0 +1,11 @@
+package githubintegration;
+
+public class project1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		
+
+	}
+
+}
---
+++ githubintegration/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module githubintegration {
+}
